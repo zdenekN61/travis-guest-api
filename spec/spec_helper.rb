e0f47b5e07fd11c5d89bc0de961ce1e09f5057b2
Ondřej Hošák <ondrej.hosak@avg.com>
2016-06-10 15:15:20 +0200

    fix: speed up storing steps into redis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,8 @@ require 'bundler/setup'
 require 'travis/guest-api/app'
 
 ENV['RAILS_ENV'] = ENV['RACK_ENV'] = ENV['ENV'] = 'test'
+
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+end
--- spec/unit/cache_spec.rb
+++ spec/unit/cache_spec.rb
@@ -8,7 +8,7 @@ describe Travis::GuestAPI::Cache do
   let(:max_job_time) { 5.minutes }
   let(:gc_polling_interval) { 5.minutes }
   let(:cache) { Travis::GuestAPI::Cache.new max_job_time, Travis.config.redis }
-  let(:test_uuid) { 'ffdec891-ac4d-4187-a228-3edbe474c775' }
+  let(:step_uuid) { 'ffdec891-ac4d-4187-a228-3edbe474c775' }
 
   after(:each) { cache.finalize }
 
@@ -16,23 +16,23 @@ describe Travis::GuestAPI::Cache do
     it 'persists given value' do
       job_id = 42
       result = { 'foo' => 'test result' }
-      cache.set job_id, test_uuid, result
-      expect(cache.get job_id, test_uuid).to eq result
+      cache.set job_id, step_uuid, result
+      expect(cache.get job_id, step_uuid).to eq result
     end
 
     it 'performs recusive update' do
       job_id = 42
-      cache.set job_id, test_uuid, 'test_data' => { v1: 1 }
-      cache.set job_id, test_uuid, 'test_data' => { v2: 2 }
-      expect(cache.get job_id, test_uuid).to eq (
+      cache.set job_id, step_uuid, 'test_data' => { v1: 1 }
+      cache.set job_id, step_uuid, 'test_data' => { v2: 2 }
+      expect(cache.get job_id, step_uuid).to eq (
         { 'test_data' => { 'v1' => 1, 'v2' => 2 } }
       )
     end
 
     it 'returns cached value' do
       job_id = 42
-      cache.set job_id, test_uuid, { 'test_data' => { 'v1' => 1 } }
-      res = cache.set job_id, test_uuid, { 'test_data' => { 'v2' => 2 } }
+      cache.set job_id, step_uuid, { 'test_data' => { 'v1' => 1 } }
+      res = cache.set job_id, step_uuid, { 'test_data' => { 'v2' => 2 } }
       expect(res).to eq (
         { 'test_data' => { 'v1' => 1, 'v2' => 2 } }
       )
@@ -40,7 +40,40 @@ describe Travis::GuestAPI::Cache do
 
     it 'throws when result is not a hash' do
       expect do
-        cache.set 42, test_uuid, 'not a hash'
+        cache.set 42, step_uuid, 'not a hash'
+      end.to raise_error ArgumentError
+    end
+  end
+
+  describe '#set_multiple' do
+    it 'persists given value' do
+      job_id = 42
+      result = { 'foo' => 'test result', 'uuid' => step_uuid }
+      cache.set_multiple job_id, [result]
+      expect(cache.get job_id, step_uuid).to eq result
+    end
+
+    it 'performs recusive update' do
+      job_id = 42
+      cache.set_multiple job_id, ['test_data' => { v1: 1 }, 'uuid' => step_uuid]
+      cache.set_multiple job_id, ['test_data' => { v2: 2 }, 'uuid' => step_uuid]
+      expect(cache.get job_id, step_uuid).to eq (
+        { 'test_data' => { 'v1' => 1, 'v2' => 2 }, 'uuid' => step_uuid }
+      )
+    end
+
+    it 'rewrites value' do
+      job_id = 42
+      cache.set_multiple job_id, ['test_data' => { v1: 1 }, 'uuid' => step_uuid]
+      cache.set_multiple job_id, ['test_data' => { v1: 2 }, 'uuid' => step_uuid]
+      expect(cache.get job_id, step_uuid).to eq (
+        { 'test_data' => { 'v1' => 2 }, 'uuid' => step_uuid }
+      )
+    end
+
+    it 'throws when result is not a hash' do
+      expect do
+        cache.set_multiple 42, step_uuid, 'not a hash'
       end.to raise_error ArgumentError
     end
   end
@@ -52,9 +85,9 @@ describe Travis::GuestAPI::Cache do
 
       it 'returns Nil' do
         job_id = 666
-        cache.set job_id, test_uuid, 'foo' => 'bar'
+        cache.set job_id, step_uuid, 'foo' => 'bar'
         sleep 0.2
-        expect(cache.get job_id, test_uuid).to be_nil
+        expect(cache.get job_id, step_uuid).to be_nil
       end
     end
 
@@ -62,8 +95,8 @@ describe Travis::GuestAPI::Cache do
       it 'returns record' do
         job_id = 666
         record = { 'foo' => 'bar' }
-        cache.set job_id, test_uuid, record
-        expect(cache.get job_id, test_uuid).to eq record
+        cache.set job_id, step_uuid, record
+        expect(cache.get job_id, step_uuid).to eq record
       end
     end
   end
@@ -71,9 +104,9 @@ describe Travis::GuestAPI::Cache do
   describe '#delete' do
     it 'deletes specified record' do
       job_id = 666
-      cache.set job_id, test_uuid, foo: 'bar'
+      cache.set job_id, step_uuid, foo: 'bar'
       cache.delete job_id
-      expect(cache.get job_id, test_uuid).to be_nil
+      expect(cache.get job_id, step_uuid).to be_nil
     end
   end
 
@@ -83,7 +116,7 @@ describe Travis::GuestAPI::Cache do
     end
 
     it "return true if job is cached" do
-      cache.set 123, test_uuid, {}
+      cache.set 123, step_uuid, {}
       expect(cache.exists?(123)).to be true
     end
   end
@@ -94,14 +127,14 @@ describe Travis::GuestAPI::Cache do
     end
 
     it "reutrns passed when any test_step was set (event without result)" do
-      cache.set 123, test_uuid, {}
+      cache.set 123, step_uuid, {}
       expect(cache.get_result(123)).to eq 'passed'
     end
 
     it "returns 'passed' to any result value expect 'failed'" do
-      cache.set 123, test_uuid, { 'result' => 'pending'}
+      cache.set 123, step_uuid, { 'result' => 'pending'}
       expect(cache.get_result(123)).to eq 'passed'
-      cache.set 123, test_uuid, { 'result' => 'broken'}
+      cache.set 123, step_uuid, { 'result' => 'broken'}
       expect(cache.get_result(123)).to eq 'passed'
     end
 
